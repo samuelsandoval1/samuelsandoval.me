59a368c8aace4ae0c04ecdcf11458aa2abf341d7
Samuel Sandoval <sjsandoval1000@gmail.com>
2021-08-18 13:19:31 -0700

    Updated formatting & added extracurriculars section

--- js/script.js
+++ js/script.js
@@ -3,31 +3,41 @@ const BLACKLISTED_KEY_CODES = [38];
 // Use \ for multi-line strings **
 const COMMANDS = {
     help:
-        'Supported commands: <span class="code">about</span>, <span class="code">education</span>, <span class="code">experience</span>, <span class="code">projects</span>, <span class="code">skills</span>, <span class="code">hobbies</span>, <span class="code">clear</span>',
+        'Supported commands: <span class="code">about</span>, <span class="code">education</span>, <span class="code">extracurriculars</span>, <span class="code">projects</span>, <span class="code">skills</span>, <span class="code">hobbies</span>, <span class="code">clear</span>',
     about:
         "Hey there! 👋🏼 <br> \
         I'm Sam, a junior  studying Computer Science at California State University, Fullerton. \
         I love programming, building projects, and teaching others about new technologies. I'm interested in Developer Advocacy and Product Management.",
 
-    skills:
-        '<span class="code">Languages:</span> C++, Python, Swift, JavaScript, TypeScript, HTML, CSS',
     education:
         '<strong class="header-name">California State University, Fullerton</strong><br>B.S. Computer Science, Expected Grad: May 2023',
+
     experience:
         '<strong class="header-name">Microsoft (May 2021 - August 2021)</strong><br><i>Explore Intern</i><br><strong class="header-name">Google (May 2020 - August 2020)</strong><br><i>STEP Intern</i>',
     // <strong class="header-name">Company (May 2020 - Aug. 2020)</strong><br><i>Title</i><br>
-    projects:
-        '<strong class="header-name">Sudoku GUI Solver</strong><br><i>A Sudoku Solver that uses the backtracking algorithm, and has a GUI to play sudoku. Built using Python, and pygame. View by typing /#sudoku-solver above in the domain.</i><br>\
-        <strong class="header-name">NiceBreakers</strong><br><i> A web application designed to make ice breakers fun! Built with Next.JS and Web Sockets. View by typing /#Nicebreakers above in the domain.</i><br>\
-        <strong class="header-name">Techish</strong><br><i>A web application to match students to mentors in tech. Built with HTML/CSS, TypeScript and Java Servlets. View by typing /#Techish above in the domain.</i><br>\
-        <strong class="header-name">Flix </strong><br><i>An iOS mobile application that allows users to browse movies now playing in theaters. Built with Xcode, Swift and the Movie Database API. View by typing /#Flix above in the domain </i><br>\
-        <strong class="header-name">Meme-Creator</strong><br><i>A web application that allows users to make memes! Built with  HTML/CSS, Javascript. View by typing /#meme-creator above in the domain. </i><br>',
+
+    extracurriculars:
+        '<strong class="header-name">TuffyHacks - CEO & Cofounder (2020-Present): </strong><br><i>Led the direction and execution for CSU Fullertons student-run hasckathon.</i><br>\
+        <strong class="header-name">acmCSUF - acmDev Director (2020-2021): </strong><br><i>Created path(committee) under acmCSUF that focused on connecting students to projects.</i>\
+        <strong class="header-name">SHPE at CSUF - Webmaster (2020-2021): </strong><br><i>Improved the website accessibility for the chapter website.</i>',
+    // <strong class="header-name">Org - Position (May 2020 - Aug. 2020)</strong><br><i>Description</i><br>
+
     hobbies:
         '- Making/Drinking Coffee Lattes <br>\
          - Doing lighting for concerts and churches. <br>\
          - Building small hacks using the latest tech on my own or at hackathons <br>\
          - Playing games like Minecraft with friends',
 
+    projects:
+        '<strong class="header-name">Sudoku GUI Solver</strong><br><i>A Sudoku Solver that uses the backtracking algorithm, and has a GUI to play sudoku. Built using Python, and pygame. View by typing /#sudoku-solver above in the domain.</i><br>\
+        <strong class="header-name">NiceBreakers</strong><br><i> A web application designed to make ice breakers fun! Built with Next.JS and Web Sockets. View by typing /#Nicebreakers above in the domain.</i><br>\
+        <strong class="header-name">Techish</strong><br><i>A web application to match students to mentors in tech. Built with HTML/CSS, TypeScript and Java Servlets. View by typing /#Techish above in the domain.</i><br>\
+        <strong class="header-name">Flix </strong><br><i>An iOS mobile application that allows users to browse movies now playing in theaters. Built with Xcode, Swift and the Movie Database API. View by typing /#Flix above in the domain </i><br>\
+        <strong class="header-name">Meme-Creator</strong><br><i>A web application that allows users to make memes! Built with  HTML/CSS, Javascript. View by typing /#meme-creator above in the domain. </i><br>',
+
+
+    skills:
+        '<span class="code">Languages:</span> C++, Python, Swift, JavaScript, TypeScript, HTML, CSS',
 };
 
 let userInput, terminalOutput;
