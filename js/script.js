86c8265ebadd40eee74f196c98ca723cfa17cfd4
Samuel Sandoval <sjsandoval1000@gmail.com>
2022-12-23 12:52:58 -0800

    Added json data in javascript temporarily

--- js/script.js
+++ js/script.js
@@ -2,7 +2,29 @@ const BLACKLISTED_KEY_CODES = [38];
 
 let userInput, terminalOutput;
 
+const COMMANDS = {
+    help:'Supported commands: <span class="code">about</span>, <span class="code">education</span>,  <span class="code">experience</span>, <span class="code">projects</span>, <span class="code">clear</span>',
+    
+    about: "Hey there! <br> I'm an undergraduate student  studying Computer Science at California State University, Fullerton (CSUF). \
+    I'm passionate about developing products and technologies that impact my community and people everywhere. <br><br> \
+    At CSUF,  you can catch me doing some work in the TSU, hanging out in the ECS Courtyard or at my favorite coffee shop in Fullerton.  \
+    While I'm not on the grind, I'll sometimes travel around, make coffee lattes or hang out with friends. <br><br>\
+    I love learning about new ideas, technology or experiences, so feel free to reach out to me. <br><br>\
+    Let's <strong><a href='https://linkedin.com/in/~samuel/'>connect</a></strong><br><br> \
+    <strong>Scroll up in the terminal.</strong>",
 
+    education: '<strong class="header-name">California State University, Fullerton</strong><br>B.S. Computer Science, Expected Grad: May 2023',
+    
+    experience: 
+        '<strong class="header-name">USAA (May 2022 - August 2022) </strong><br><i>Digital/Technical Product Manager Intern</i><br>\ <strong class="header-name">  Microsoft (May 2021 - August 2021) </strong><br><i>Explorer Intern</i><br>\
+        <strong class="header-name"> Google (May 2020 - August 2020) </strong><br><i>STEP Intern</i><br></br>',
+    
+    projects: 
+    '<strong class="header-name">TuffyHacks.com</strong><br>Product Manager<br><i> Lead a cross-functional team for the MVP development of the TuffyHacks.com. Some of my product work included: creating product requirements, communicating with stakeholders and translating requirements into user stories. TuffyHacks.com is a platform that allows hackers to have an interactive experience at the TuffyHacks hackathon. View <strong><a href="https://tuffyhacks.com">here</a></strong>.</i><br> \
+    <strong class="header-name">GP-Aide App </strong><br>Product Manager<br><i> Drove the MVP development with a team of other students. Some of my product work included: created product specs, managed the product sprint progress, and contributed to the technical development of the application. GP-Aide an iOS mobile application that allows users to calculate their semester grade point average. View <strong><a href="https://github.com/samuelsandoval1/223w-gp-aide">here</a></strong>.</i><br> <strong class="header-name">NiceBreakers </strong><br>Product Manager <br><i>Lead the product efforts to design product specs, create UX wireframes, and develop this idea into an MVP. Nicebreakers provides space for individuals to play games and get to know more about each other. View <strong><a href="https://devpost.com/software/nicebreakers-8hwuoe">here</a></strong>.</i><br> \
+    <strong class="header-name">Personal API </strong><br>Personal Project<br><i> An API designed to display information about Samuel Sandoval. This website is fetching from this API. Built using Python and Flask. View <strong><a href="http://github.com/samuelsandoval1/api.samuelsandoval.me">here</a></strong>.</i><br> \
+    </br> <b>Scroll up in the terminal.</b> ',
+}
 
 const app = () => {
     userInput = document.getElementById('userInput');
@@ -47,43 +69,46 @@ const execute = async function executeCommand(input) {
         location.reload();
     }
     else {
-        if (input.includes("help")) { // works
-            var modifiedInput = input += "_message";
-            const result = await fetchResults(modifiedInput.toLowerCase());
+        // if (input.includes("help")) { // works
+        //     var modifiedInput = input += "_message";
+        //    const result = await fetchResults(modifiedInput.toLowerCase());
         //    console.log("result from fetchResults "+result);
-            output += result.text;
-        }
-        else if (input.includes("about")) {
-            const result = await fetchResults(input.toLowerCase());
-            output += result.text;
-        }
-        else if (input.includes("education")) {
-            const result = await fetchResults(input.toLowerCase());
-            output += result.text;
-        }
-        else if (input.includes("experience")) {
-            const result = await fetchResults(input.toLowerCase());
-            output += result.exp1;
-            output += result.exp2;
-            output += result.exp3;
-        }
-        else if (input.includes("the gospel")) {
-            const result = await fetchResults("gospel");
-            for (const textField in result){
-                output += result.text;
-            }
-        }
-        else if (input.includes("projects")) {
-            const result = await fetchResults(input.toLowerCase());
-            output += result.project1;
-            output += result.project2;
-            output += result.project3;
-            output += result.project4;
-            output += result.scroll;
-        }
-        else{
+        //     output += result.text;
+        // }
+        // else if (input.includes("about")) {
+        //     const result = await fetchResults(input.toLowerCase());
+        //     output += result.text;
+        // }
+        // else if (input.includes("education")) {
+        //     const result = await fetchResults(input.toLowerCase());
+        //     output += result.text;
+        // }
+        // else if (input.includes("experience")) {
+        //     const result = await fetchResults(input.toLowerCase());
+        //     output += result.exp1;
+        //     output += result.exp2;
+        //     output += result.exp3;
+        // }
+        // else if (input.includes("the gospel")) {
+        //     const result = await fetchResults("gospel");
+        //     for (const textField in result){
+        //         output += result.text;
+        //     }
+        // }
+        // else if (input.includes("projects")) {
+        //     const result = await fetchResults(input.toLowerCase());
+        //     output += result.project1;
+        //     output += result.project2;
+        //     output += result.project3;
+        //     output += result.project4;
+        //     output += result.scroll;
+        // }
+        if (!COMMANDS.hasOwnProperty(input)){
             output += `<div class="terminal-line"> ${input}: command not found</div>`;
             console.log('Oops! no such command');
+        }
+        else{
+            output += COMMANDS[input];
         }
     }
     terminalOutput.innerHTML = `${terminalOutput.innerHTML}<div class="terminal-line">${output}</div>`;
